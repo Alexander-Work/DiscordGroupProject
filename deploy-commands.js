39ec41374807c8f2b7769ad2e849b77b101ba178
MD SHARIFUL ISLAM <120353767+200495587@users.noreply.github.com>
2023-07-25 13:35:45 -0400

    localized

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,5 +1,5 @@
-const { REST } = require('discord.js');
-const { clientId, token } = require('./config.json');
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
 const fs = require('fs');
 const path = require('path');
 
@@ -9,39 +9,40 @@ const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	// Grab all the command files from the commands directory you created earlier
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-
-    if ('data' in command && 'execute' in command) {
-      commands.push(command.data); // Directly push the command data object
-      console.log(command.data);
-    } else {
-      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-    }
-  }
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+            console.log(command.data.toJSON());
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST().setToken(token);
 
 // and deploy your commands!
 (async () => {
-  try {
-    console.log(`Started refreshing ${commands.length} application (/) commands.`);
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-    // The put method is used to fully refresh all commands
-    const data = await rest.put(
-      `/applications/${clientId}/guilds/1130296090114134119/commands`, // Replace '1130300934740791336' with your actual numeric guild ID
-      { body: commands },
-    );
+		// The put method is used to fully refresh all commands
+		const data = await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: commands },
+		);
 
-    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-  } catch (error) {
-    // And of course, make sure you catch and log any errors!
-    console.error(error);
-  }
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
 })();
